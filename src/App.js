2c6f321428af5f9952691afd1ccb0f1bfd485409
Ranganayaki Kolanupaka <93740204+rkolanup@users.noreply.github.com>
2022-03-22 15:30:24 -0700

    Add API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import {Container} from 'react-bootstrap';
+import RecipieCard from './components/RecipieCard/RecipieCard';
+import Searchform from './components/Searchform/Searchform';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [, setError] = useState();
+  const [, setSearch] = useState('');
+
+  const sendSearchToApp = (food)=>{   
+    setSearch(food);
+
+    fetch(`https://api.edamam.com/search?q=${food}&app_id=${process.env.REACT_APP_CLIENT_ID}&app_key=${process.env.REACT_APP_MY_API_KEY}&from=0&to=100`)
+      .then((response)=>response.json())
+      .then((data)=> {
+        console.log('Hello: ',data)
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+      
+    setSearch('');   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Container fluid className = "App app-wrapper">
+    <div className = "recipe-wrapper">
+      <h1 className = "app-title">Welcome to Food App</h1>
+      <Searchform sendSearchToApp={sendSearchToApp}/>
+      <RecipieCard data={data}/>
     </div>
+  </Container>  
   );
 }
 
